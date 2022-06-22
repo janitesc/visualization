72590cc5696733907aacf8cfedc49279ced444f4
janitesc <107386340+janitesc@users.noreply.github.com>
2022-06-22 12:46:56 -0400

    Geneviewer works fully
    YES

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,18 +1,49 @@
 <script lang="ts">
+
   // TODO: implement GeneViewer by calling into the /genes api endpoint to get data
   // TODO: add a MolViewer along the same lines
   import GeneViewer from "./components/GeneViewer.svelte";
+  import { getGenes, Genes } from "./api";
+  import { onMount } from "svelte";
+  let lengthdata;
+  let genesdata = [];
+  onMount(async () => {
+  const genes = getGenes();
+  await getGenes();
+  genes.then((data) => {
+    lengthdata = data.length;
+    genesdata = data.genes;
+  })
+});
 </script>
 
 <main>
-  <GeneViewer width=200 height=200/>
+  GENEVIEWER
   <div>
-    <GeneViewer width=600 height=300/>
-    <GeneViewer width=600 height=300/>
+    <GeneViewer length = {lengthdata} genedata = {genesdata} />
   </div>
   <div id="last">
-    <GeneViewer width=1200 height=400/>
+    <svg width="1500" height="100">
+      <line x1="100" x2="1400" y1="10" y2="10"></line>
+      <text x="100" y = "0">0</text>
+      <text x="230" y = "0">{Math.round(lengthdata/10)}</text>
+        <text x="360" y = "0">{Math.round((lengthdata/10))*2}</text>
+        <text x="490" y = "0">{Math.round((lengthdata/10))*3}</text>
+        <text x="620" y = "0">{Math.round((lengthdata/10))*4}</text>
+        <text x="750" y = "0">{Math.round((lengthdata/10))*5}</text>
+        <text x="880" y = "0">{Math.round((lengthdata/10))*6}</text>
+        <text x="1010" y = "0">{Math.round((lengthdata/10))*7}</text>
+        <text x="1140" y = "0">{Math.round((lengthdata/10))*8}</text>
+        <text x="1270" y = "0">{Math.round((lengthdata/10))*9}</text>
+        <text x="1400" y = "0">{lengthdata}</text>
+      <g class="x">
+       
+        
+      </g> 
+      </svg>
   </div>
+ Length: {lengthdata}
+
 </main>
 
 <style>
@@ -34,6 +65,22 @@
   }
 
   #last {
-    width: 900px;
+    
+
   }
+  svg {
+    align-self: center;
+		overflow: visible;
+		margin: 3em;
+	}
+	
+	line, polyline { 
+    align-self: baseline;
+		fill: none;
+		stroke: black;
+	}
+	
+	.x text {
+		text-anchor: left;
+	}
 </style>
--- web/src/components/GeneViewer.svelte
+++ web/src/components/GeneViewer.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import { getGenes, Genes } from "../api.ts";
-
-  export let width: number;
-  export let height: number;
-
+  export let length: number;
+  export let genedata = [];
+  debugger;
+  console.log(genedata);
   console.debug(getGenes());
+  let colors = ["Lavender", "Plum", "Orchid","Fuchsia","MediumOrchid","DarkOrchid","BlueViolet","Purple","MediumSlateBlue","DarkSlateBlue","RebeccaPurple","Indigo","MidnightBlue", "MediumBlue","RoyalBlue", "DeepSkyBlue", "CornflowerBlue","SkyBlue", "PowderBlue","LightSteelBlue"];
 </script>
 
 <style>
@@ -20,10 +21,37 @@
     align-items: center;
     flex-direction: column;
   }
+  svg {
+    align-self: center;
+		overflow: visible;
+		margin: 3em;
+	}
+	
+	line, polyline { 
+    align-self: baseline;
+		fill: none;
+		
+    stroke: black;
+	}
+  rect { 
+    align-self: baseline;
+    border-end-end-radius: 80px 80px;
+		stroke: rgb(0, 0, 0);
+	}
 </style>
 
+
+
+
 <div>
-  <h1>Genes</h1>
-  <canvas width={width} height={height}>
-  </canvas>
+  <svg width="1500" height="300">
+    <line x1="100" x2=1400 y1="50" y2="50"></line>
+    {#each genedata as Array,i}
+    <rect x={[(Array[0]/length)*1300+100]} y="0" width={[(Array[1]/length)*1300+100]-[[(Array[0]/length)*1300+100]]} height="100" rx="10" ry="10" id="Motif [i]"style="fill:{[colors[i]]};" />
+    <rect x="1400" y={[(Array[0]/length)*400+20]} width={10} height="10" rx="5" ry="5" id="Motif [i]" style="fill:{[colors[i]]};">
+      <title>Motif {i+1}</title>
+    </rect> />
+    <text x="1415" y={[(Array[0]/length)*400+30]}>Motif {i+1} </text>
+    {/each}
+  </svg>
 </div>
