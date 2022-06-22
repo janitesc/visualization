e24dd7710be1e74cc7139e931b89c68d75359127
janitesc <107386340+janitesc@users.noreply.github.com>
2022-06-22 13:25:03 -0400

    adjusted geneviewer a bit

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -10,6 +10,8 @@
   onMount(async () => {
   const genes = getGenes();
   await getGenes();
+  //for multiple genes, I would make another array and make an
+  //array of genes
   genes.then((data) => {
     lengthdata = data.length;
     genesdata = data.genes;
@@ -19,23 +21,27 @@
 
 <main>
   GENEVIEWER
+  <!-- For multiple genes, I would get the maximum length
+  of the gene array, and use that one to scale the x-axis, so the 
+  graph was neat and easy to read.  -->
   <div>
     <GeneViewer length = {lengthdata} genedata = {genesdata} />
+    
   </div>
   <div id="last">
     <svg width="1500" height="100">
-      <line x1="100" x2="1400" y1="10" y2="10"></line>
-      <text x="100" y = "0">0</text>
-      <text x="230" y = "0">{Math.round(lengthdata/10)}</text>
-        <text x="360" y = "0">{Math.round((lengthdata/10))*2}</text>
-        <text x="490" y = "0">{Math.round((lengthdata/10))*3}</text>
-        <text x="620" y = "0">{Math.round((lengthdata/10))*4}</text>
-        <text x="750" y = "0">{Math.round((lengthdata/10))*5}</text>
-        <text x="880" y = "0">{Math.round((lengthdata/10))*6}</text>
-        <text x="1010" y = "0">{Math.round((lengthdata/10))*7}</text>
-        <text x="1140" y = "0">{Math.round((lengthdata/10))*8}</text>
-        <text x="1270" y = "0">{Math.round((lengthdata/10))*9}</text>
-        <text x="1400" y = "0">{lengthdata}</text>
+      <line x1="50" x2="1350" y1="10" y2="10"></line>
+      <text x="50" y = "0">0</text>
+      <text x="180" y = "0">{Math.round(lengthdata/10)}</text>
+        <text x="310" y = "0">{Math.round((lengthdata/10))*2}</text>
+        <text x="440" y = "0">{Math.round((lengthdata/10))*3}</text>
+        <text x="570" y = "0">{Math.round((lengthdata/10))*4}</text>
+        <text x="700" y = "0">{Math.round((lengthdata/10))*5}</text>
+        <text x="830" y = "0">{Math.round((lengthdata/10))*6}</text>
+        <text x="960" y = "0">{Math.round((lengthdata/10))*7}</text>
+        <text x="1090" y = "0">{Math.round((lengthdata/10))*8}</text>
+        <text x="1220" y = "0">{Math.round((lengthdata/10))*9}</text>
+        <text x="1350" y = "0">{lengthdata}</text>
       <g class="x">
        
         
--- web/src/components/GeneViewer.svelte
+++ web/src/components/GeneViewer.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
   import { getGenes, Genes } from "../api.ts";
+import App from "../App.svelte";
   export let length: number;
   export let genedata = [];
   debugger;
   console.log(genedata);
   console.debug(getGenes());
+  //In a bigger group of genes, I would autogenerate this and call however
+  //many colors were needed, but a smaller sample meant
+  //I could pick the colors, leaving with 20 unique modules
+  //that can be seen
   let colors = ["Lavender", "Plum", "Orchid","Fuchsia","MediumOrchid","DarkOrchid","BlueViolet","Purple","MediumSlateBlue","DarkSlateBlue","RebeccaPurple","Indigo","MidnightBlue", "MediumBlue","RoyalBlue", "DeepSkyBlue", "CornflowerBlue","SkyBlue", "PowderBlue","LightSteelBlue"];
 </script>
 
@@ -36,22 +41,21 @@
   rect { 
     align-self: baseline;
     border-end-end-radius: 80px 80px;
+    fill:Aquamarine;
 		stroke: rgb(0, 0, 0);
 	}
 </style>
-
-
-
-
 <div>
+  <!-- For multiple genes, I would space them out a bit closer, and only repeat motifs
+  one time-->
   <svg width="1500" height="300">
-    <line x1="100" x2=1400 y1="50" y2="50"></line>
+    <line x1="50" x2=1350 y1="50" y2="50"></line>
     {#each genedata as Array,i}
-    <rect x={[(Array[0]/length)*1300+100]} y="0" width={[(Array[1]/length)*1300+100]-[[(Array[0]/length)*1300+100]]} height="100" rx="10" ry="10" id="Motif [i]"style="fill:{[colors[i]]};" />
-    <rect x="1400" y={[(Array[0]/length)*400+20]} width={10} height="10" rx="5" ry="5" id="Motif [i]" style="fill:{[colors[i]]};">
+    <rect x={[(Array[0]/length)*1300+50]} y="0" width={[(Array[1]/length)*1300+100]-[[(Array[0]/length)*1300+100]]} height="100" rx="10" ry="10" id="Motif [i]"style="fill:{[colors[i]]};" />
+    <rect x="1365" y={i*30} width={15} height="15" rx="3" ry="3" id="Motif [i]" style="fill:{[colors[i]]};">
       <title>Motif {i+1}</title>
     </rect> />
-    <text x="1415" y={[(Array[0]/length)*400+30]}>Motif {i+1} </text>
+    <text x="1384" y={i*30 + 13}>Motif {i+1} </text>
     {/each}
   </svg>
 </div>
